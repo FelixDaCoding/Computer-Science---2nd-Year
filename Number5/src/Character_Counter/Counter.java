405184f177fb6f8911718209cf74af7613254338
FelixDaCoding <166514050+FelixDaCoding@users.noreply.github.com>
2025-11-17 09:29:20 +0800

    Add files via upload

---
+++ Number5/src/Character_Counter/Counter.java
@@ -0,0 +1,28 @@
+package Character_Counter;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Counter extends JFrame{
+    private JTextField characterTextField;
+    private JButton countButton;
+    private JPanel CharacterCounter;
+    private JTextArea messageTextArea;
+    private JLabel labelist;
+
+
+    public Counter() {
+
+        countButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Number5/src/Character_Counter/Main.java
@@ -0,0 +1,13 @@
+package Character_Counter;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Character Counter");
+        frame.setSize(420,420);
+        frame.setVisible(true);
+    }
+}
